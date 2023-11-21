e315e1a7b234beddb240e666bd5f5a03bfe573de
unknown <rakibulhasan.seu.cse@gmail.com>
2023-11-21 19:41:44 +0600

    2 problems solved in codeforces

---
+++ codeforces/Random codeforces problems/A. Game with Board.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        if(n>=5) cout << "Alice\n";
+        else cout << "Bob\n";
+    }
+
+
+    return 0;
+}
