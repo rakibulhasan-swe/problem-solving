bf92d3d1b86f1bc8f977d4bf1685ff2eb5cd8906
unknown <rakibulhasan.seu.cse@gmail.com>
2023-11-23 22:58:49 +0600

    codechef contest

---
+++ codechef/Starters 109 (Rated till 5 Stars)/Worth of a Video.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    int n;
+	    cin >> n;
+	    
+	    cout << n*24000 << "\n";
+	}
+	
+	return 0;
+}
