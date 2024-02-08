50d16478a21702efa246e465b8de37f73d52a97a
unknown <rakibulhasan.seu.cse@gmail.com>
2024-02-08 22:16:42 +0600

    codechef contest

---
+++ codechef/Starters 120 Division 3 (Rated)/Valentine Gifts.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int n;
+        cin >> n;
+
+        if(n >= 350) cout << "YES\n";
+        else cout << "NO\n";
+    }
+
+    return 0;
+}
